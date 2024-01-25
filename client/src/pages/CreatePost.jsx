5467e1e4bd0cebe21d62e5e3a6115d06361ebf5b
ranasingh1 <ranasingh061177@gmail.com>
2024-01-25 05:54:28 +0530

    Hoisting

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -51,7 +51,7 @@ const CreatePost = () => {
           setLoading(true);
 
             try {
-              const response = await fetch('http://localhost:8080/api/v1/post', {
+              const response = await fetch('https://dalle-clone-5f9e.vercel.app/api/v1/post', {
                 method: 'POST',
                 headers:{
                   'Content-Type' : "application/json",
--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+// Use cors middleware with specific options
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://dalle-clone-liart.vercel.app/'],// Replace with your client's origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  optionsSuccessStatus: 204,
+}));
+
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
